fcc5c7f422b7ddd1a511e12ce615275baccc37bb
VinceZCL <vincewcode@gmail.com>
2022-02-02 19:16:59 +0800

    minor changes

--- bin_decode.py
+++ bin_decode.py
@@ -3,17 +3,6 @@ import sys
 fraw = []
 result = []
 
-"""
-0 - 128 i0
-1 - 64 i1
-0 - 32 i2
-0 - 16 i3
-0 - 8 i4
-0 - 4 i5
-0 - 2 i6
-1 - 1 i7
-"""
-
 def check(data):
     asc = 0
     if int(data[0]):
